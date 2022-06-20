30f9398a329b00145096a6f20a7812debd17b5fc
Seyed Hassan Jabbekhaneh <jabbekhaneh@gmail.com>
2022-06-21 00:40:47 +0430

    create project vue cli and config tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,248 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- topBar -->
+    <header class="topBar relative top-0 w-full h-12"></header>
+
+    <!-- container -->
+    <section class="container mx-auto mt-28">
+      <!-- searchBox -->
+      <div
+        class="searchBox flex items-center justify-around w-full h-20 rounded-md"
+      >
+        <!-- search -->
+        <div>
+          <label class="w-40 h-5">Search by release date:</label>
+          <input type="search" placeholder="search" class="w-56 h-8 ml-7" />
+        </div>
+        <!-- submit -->
+        <input type="submit" value="submit" class="btn-primary-search" />
+      </div>
+
+      <!-- result -->
+      <div class="result mt-10 grid grid-cols-3 gap-6">
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card -->
+        <div class="card h-48">
+          <div class="card-body flex flex-row">
+            <div class="img-wrapper w-40 h-48 flex">
+              <img src="../assets/img/01.png" class="" alt="img title" />
+            </div>
+
+            <div class="py-2 ml-10 grid grid-cols-1">
+              <h1 class="text-lg font-bold">Inception</h1>
+
+              <div class="">
+                <span class="mt-5"
+                  ><i class="_icon _icon-calendar mr-2 mb-2"></i
+                  >2014-10-22</span
+                >
+                <ul class="flex gap-6">
+                  <li class="text-sm font-light">Action</li>
+                  <li class="text-sm font-light">Adventure</li>
+                  <li class="text-sm font-light">Sci-Fi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-96 mx-auto pagination container">
+        <div class="flex justify-around">
+          <a href="">Next Page</a>
+          <span class="border-l-2 border-gray-500"></span>
+          <a href="" class="text-blue-700">Previous page</a>
+        </div>
+        <span class="flex justify-center text-gray-400"
+          >Showing results 1-20</span
+        >
+      </div>
+    </section>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+<style>
+li {
+  list-style: square;
+}
+
+.topBar {
+  background: #11b980;
+}
+.searchBox {
+  background: #cfcfcf;
+}
+.card {
+  border-radius: 0.25rem;
+  background: #f1f1f1;
+}
+
+.btn-primary-search {
+  background: #549df2 !important;
+  border-radius: 100px;
+  width: 74px;
+  height: 33px;
+  cursor: pointer;
+}
+</style>
